ad9e61b937ce9299e21bb2a497f6713be54d6369
Taz <tsyroid@gmail.com>
2024-12-30 21:29:52 -0800

    Add doom/bin to path

--- fish/config.fish
+++ fish/config.fish
@@ -19,7 +19,7 @@ fish_add_path /opt/homebrew/bin # macOS
 fish_add_path /opt/homebrew/sbin # macOS
 fish_add_path /opt/homebrew/opt/gawk/libexec/gnubin # macOS; brew
 fish_add_path /opt/homebrew/opt/gnu-tar/libexec/gnubin # macOS; brew
-# fish_add_path $HOME/.config/emacs/bin/ # Doom Emacs
+fish_add_path $HOME/.config/emacs/bin/ # Doom Emacs
 
 # if postgres
 # fish_add_path /Applications/Postgres.app/Contents/Versions/latest/bin
