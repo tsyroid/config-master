89eceee27a10ec9defd8fb46cdf018338f20c5d6
Taz <tsyroid@gmail.com>
2025-01-31 18:44:04 -0800

    Add `gnu-coreutils` to path (FIX: emacs protest)

--- fish/config.fish
+++ fish/config.fish
@@ -19,6 +19,7 @@ fish_add_path /opt/homebrew/bin # macOS
 fish_add_path /opt/homebrew/sbin # macOS
 fish_add_path /opt/homebrew/opt/gawk/libexec/gnubin # macOS; brew
 fish_add_path /opt/homebrew/opt/gnu-tar/libexec/gnubin # macOS; brew
+fish_add_path /opt/homebrew/opt/coreutils/libexec/gnubin # macOS; brew
 fish_add_path $HOME/.config/emacs/bin/ # Doom Emacs
 
 # if postgres
