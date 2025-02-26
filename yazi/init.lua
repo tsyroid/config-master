c12fb52debc7395ddc22f4b8d3cc646e33502242
Taz <tsyroid@gmail.com>
2025-02-26 08:23:50 -0800

    Yazi Rebuild (to fix ongoing, longstanding WTFs)

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,6 +1,16 @@
 -- ~/.config/yazi/init.lua
+-- <2025-02-11 Tue 07:53>
 
-require('full-border'):setup {
-  -- available values: ui.border.PLAIN, ui.border.ROUNDED
-  type = ui.Border.ROUNDED,
-}
+require('full-border'):setup()
+
+-- show symlinks in file status bar
+-- https://yazi-rs.github.io/docs/tips#symlink-in-status
+
+Status:children_add(function(self)
+  local h = self._current.hovered
+  if h and h.link_to then
+    return ' -> ' .. tostring(h.link_to)
+  else
+    return ''
+  end
+end, 3300, Status.LEFT)
